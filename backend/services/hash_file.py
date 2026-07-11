90541e92fda64774c2a31a87354de1bc26057016
Krrish-Dangi <krrishdangi2005@gmail.com>
2026-07-11 11:11:36 +0530

    added a file verification statement in hash_file.py

--- backend/services/hash_file.py
+++ backend/services/hash_file.py
@@ -1,7 +1,10 @@
 import hashlib
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
 
 def hash_file_bytes(file: UploadFile):
-    content = file.file.read()
-    file_hash = hashlib.sha256(content).hexdigest()
-    return file_hash
+    if file.filename.lower().endswith(".pdf"):
+        content = file.file.read()
+        file_hash = hashlib.sha256(content).hexdigest()
+        return file_hash
+    
+    raise HTTPException(status_code=400, detail="Please upload PDFs only...")
